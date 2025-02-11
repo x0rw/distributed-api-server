49db2bbec699915f2cef05d810b036d992ecc091
x0rw <mahdi.svt5@gmail.com>
2025-02-11 17:08:37 +0100

    further improvments

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,8 @@
-use std::env::var;
-use std::io::prelude::*;
 use std::io::Error;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::ptr::write;
-use std::{thread, time::Duration};
 mod error;
 mod http_handler;
 mod routes;
 use error::Result;
-use http_handler::*;
-use routes::RoutesMap;
 use tcp_server::TcpServer;
 mod controller;
 mod tcp_server;
@@ -22,6 +14,6 @@ fn main() -> Result<()> {
         .load("/article", "res/article.html")
         .error_page("404", "res/404.html")
         .add_controller("/art", controller::Controller::ArticleController);
-    let server = TcpServer::new("127.0.0.1:1111".to_string(), pub_routes).launch();
+    TcpServer::new("127.0.0.1:1111".to_string(), pub_routes).launch()?;
     Ok(())
 }
