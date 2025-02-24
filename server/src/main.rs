d61b4796d88f39907887f1f432b7ad0e60578475
x0rw <mahdi.svt5@gmail.com>
2025-02-24 15:52:16 +0000

    basic async implementation(not tested)

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,39 @@
-use std::io::Error;
+use std::mem;
+use std::pin::Pin;
+use std::{io::Error, ops::DerefMut};
 mod error;
 mod routes;
 use error::Result;
 
-mod main_async;
 use tcp_server::TcpServer;
+use tcp_server_async::AsyncTcpServer;
 mod auth;
 mod controller;
 mod http;
 use crate::http::{builder, handler, header};
+mod main_async;
 mod tcp_server;
 mod tcp_server_async;
 mod utils;
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut pub_routes = routes::RoutesMap::new();
+    pub_routes
+        .load("/", "res/index.html", handler::HttpMethod::POST)
+        .load("/article", "res/article.html", handler::HttpMethod::GET)
+        .error_page("404", "res/404.html", handler::HttpMethod::GET)
+        .add_controller(
+            "/echo",
+            controller::Controller::EchoController,
+            handler::HttpMethod::GET,
+        );
+    AsyncTcpServer::new("127.0.0.1:1111".to_string(), pub_routes)
+        .await?
+        .launch()
+        .await;
+    Ok(())
+}
+/*
 fn main() -> Result<()> {
     let mut pub_routes = routes::RoutesMap::new();
     pub_routes
@@ -26,3 +48,4 @@ fn main() -> Result<()> {
     TcpServer::new("127.0.0.1:1111".to_string(), pub_routes).launch()?;
     Ok(())
 }
+*/
--- server/src/tcp_server_async.rs
+++ server/src/tcp_server_async.rs
@@ -1,58 +1,59 @@
 use crate::{builder::HttpBuilder, handler::handle_http, routes::RoutesMap, Result};
 use std::io::{Read, Write};
+use std::sync::Arc;
 use tokio::io::{AsyncBufRead, AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 
-pub struct TcpServer {
+pub struct AsyncTcpServer {
     hostaddr: String,
     listener: TcpListener,
-    routes: RoutesMap,
+    routes: Arc<RoutesMap>,
 }
 
-impl TcpServer {
+impl AsyncTcpServer {
     pub async fn new(hostaddr: String, routes: RoutesMap) -> Result<Self> {
         Ok(Self {
             hostaddr: "moved".to_string(),
             listener: TcpListener::bind(hostaddr).await?,
-            routes,
+            routes: Arc::new(routes),
         })
     }
-    pub async fn launch(&self) -> Result<()> {
+    pub async fn launch(self) -> Result<()> {
         loop {
             let (mut stream, _sockaddr) = self.listener.accept().await.unwrap();
-            tokio::spawn(self.handle_client(stream));
+            let arc_route = Arc::clone(&self.routes);
+            tokio::spawn(async_handle_client(arc_route, stream));
         }
     }
+}
+// all read and write sys calls should be done here
+// errors propagated from this layer are crucial
+async fn async_handle_client(route: Arc<RoutesMap>, mut stream: TcpStream) -> Result<()> {
+    println!("Client Connected");
+    let mut buffer = [0; 1000];
+    stream.read(&mut buffer).await?;
+    let buffer_utf8 = String::from_utf8_lossy(&buffer[..]).to_string();
 
-    // all read and write sys calls should be done here
-    // errors propagated from this layer are crucial
-    async fn handle_client(&self, mut stream: TcpStream) -> Result<()> {
-        println!("Client Connected");
-        let mut buffer = [0; 1000];
-        stream.read(&mut buffer).await?;
-        let buffer_utf8 = String::from_utf8_lossy(&buffer[..]).to_string();
+    // println!("{}", buffer_utf8.to_string());
 
-        // println!("{}", buffer_utf8.to_string());
+    let handler = match handle_http(&buffer_utf8) {
+        Ok(e) => e,
+        Err(e) => {
+            stream
+                .write(HttpBuilder::build_badrequest().as_bytes())
+                .await?;
+            println!("client handler error {}", e);
+            return Ok(()); // errors in handle_http arent that serious
+        }
+    };
+    let uri = &handler.req_line.uri;
+    let route = route.get(uri);
+    let http_response = HttpBuilder::new(handler, route).build();
 
-        let handler = match handle_http(&buffer_utf8) {
-            Ok(e) => e,
-            Err(e) => {
-                stream
-                    .write(HttpBuilder::build_badrequest().as_bytes())
-                    .await?;
-                println!("client handler error {}", e);
-                return Ok(()); // errors in handle_http arent that serious
-            }
-        };
-        let uri = &handler.req_line.uri;
-        let route = self.routes.get(uri);
-        let http_response = HttpBuilder::new(handler, route).build();
-
-        let stream_send = stream.write(http_response.as_bytes()).await?;
-        println!("{stream_send} Bytes sent to the client");
-        Ok(())
-    }
+    let stream_send = stream.write(http_response.as_bytes()).await?;
+    println!("{stream_send} Bytes sent to the client");
+    Ok(())
 }
 
 #[tokio::main]
